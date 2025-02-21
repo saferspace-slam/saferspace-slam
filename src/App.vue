b56dc23fb8d6b67753c035f897bc08aeb19cb99a
max <mail@maxgietl.de>
2025-02-21 18:31:42 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+      <nav class="flex gap-3 justify-center mt-3">
+        <RouterLink class="text-normal-text" to="/">Home</RouterLink>
+        <RouterLink class="text-normal-text" to="/about">About</RouterLink>
+      </nav>
+  </header>
+
+  <main class="mt-2 mx-4">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+nav a.router-link-exact-active {
+  color: var(--color-special-text);
+}
+</style>
