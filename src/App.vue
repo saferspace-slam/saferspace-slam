dc06017e8ffa26213964b47e6168019d79c0b156
max <mail@maxgietl.de>
2025-02-22 03:25:37 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView, useRoute } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { computed, onMounted, reactive, ref, watch } from 'vue';
-import Instagram from './components/Instagram.vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import { store } from './store';
-
 
 window.addEventListener('scroll', () => {
   store.scrolled = window.scrollY > 50;
@@ -30,29 +27,34 @@ watch(navActive, () => {
   if (navActive.value) {
     mobileNav.value.style.marginTop = 0;
   } else {
+    console.log(mobileNav.value.offsetHeight);
     mobileNav.value.style.marginTop = `-${mobileNav.value.offsetHeight}px`;
   }
 });
+const loading = ref(true);
 onMounted(() => {
+  console.log(mobileNav.value.style);
   mobileNav.value.style.marginTop = `-${mobileNav.value.offsetHeight}px`;
+  console.log(mobileNav.value.style);
+  setTimeout(() => {
+    loading.value = false;
+  }, 1)
 })
 
 
-const menuItemClasses = "transition-transform duration-500 text-menu-item hover:opacity-80 hover:border-b-1 hover:border-b-white/80"
+const menuItemClasses = "transition-transform duration-500 text-menu-item hover:opacity-80 outline-hover"
 </script>
 
 <template>
   <div class="flex flex-col justify-between min-h-screen" @click="navActive = false">
-    <header class="transition-colors duration-500 z-50 w-full top-0 lg:bg-menu-background flex justify-center"
-      :class="{ 'not-hover:lg:bg-transparent': darkenNav, 'fixed': onHome, 'sticky': !onHome }">
+    <header class="z-50 w-full top-0 lg:bg-menu-background flex justify-center"
+      :class="{ 'not-hover:lg:bg-transparent': darkenNav, 'fixed': onHome, 'sticky': !onHome, 'transition-colors duration-500': !loading }">
 
-      <div
-        class="transition-all ease-in-out duration-500 w-full flex justify-between max-w-[1200px] lg:items-center flex-col lg:flex-row"
-        :class="{ 'lg:py-5': store.scrolled, 'lg:py-7': !store.scrolled }">
+      <div class="w-full flex justify-between max-w-[1200px] lg:items-center flex-col lg:flex-row"
+        :class="{ 'lg:py-5': store.scrolled, 'lg:py-7': !store.scrolled, 'transition-all ease-in-out duration-500': !loading }">
 
-        <div
-          class="z-10 gap-6 transition-colors duration-500 not-lg:py-4 px-5 flex justify-between items-center w-full not-lg:bg-menu-background"
-          :class="{ 'not-hover:not-lg:bg-transparent': darkenNav }">
+        <div class="z-10 gap-6 not-lg:py-4 px-5 flex justify-between items-center w-full not-lg:bg-menu-background"
+          :class="{ 'not-hover:not-lg:bg-transparent': darkenNav, 'transition-colors duration-500': !loading }">
           <RouterLink to="/">
             <img src="@/assets/logo_white.svg" class="h-5" />
           </RouterLink>
@@ -70,16 +72,28 @@ const menuItemClasses = "transition-transform duration-500 text-menu-item hover:
           </button>
         </div>
 
-        <nav ref="mobileNav"
-          class="lg:hidden transition-all duration-500 h-full py-7 px-5 gap-7 justify-end flex-col flex bg-menu-background"
-          :class="{ '!flex opacity-100': navActive, 'opacity-0': !navActive }">
+        <nav ref="mobileNav" class="h-full py-7 px-5 gap-7 justify-end flex-col flex bg-menu-background" :class="{
+          '!flex opacity-100': navActive, 'opacity-0': !navActive, 'lg:hidden transition-all duration-500': !loading
+        }">
           <RouterLink v-for="route in routes" :class="menuItemClasses" :to="route[0]" @click="navActive = false">{{
             route[1] }}</RouterLink>
+          <a :class="menuItemClasses" href="https://www.instagram.com/saferspace_slam/" target="_blank"
+            @click="navActive = false">
+            <div class="flex gap-[10px] items-center">
+              <img src="@/assets/instagram_logo_simple.svg" class="h-5 ml-[2px] invert" />
+              <p>saferspace_slam</p>
+            </div>
+          </a>
         </nav>
 
-        <nav class="not-lg:hidden transition-all duration-500 h-full px-5 gap-7 justify-end flex-row flex items-center">
+        <nav class="not-lg:hidden h-full px-5 gap-7 justify-end flex-row flex items-center"
+          :class="{ 'duration-500 transition-all': !loading }">
           <RouterLink v-for="route in routes" :class="menuItemClasses" :to="route[0]" @click="navActive = false">{{
             route[1] }}</RouterLink>
+          <a :class="menuItemClasses" href="https://www.instagram.com/saferspace_slam/" target="_blank"
+            @click="navActive = false">
+            <img src="@/assets/instagram_logo_simple.svg" class="h-5 mr-5 invert" />
+          </a>
         </nav>
 
       </div>
@@ -90,15 +104,15 @@ const menuItemClasses = "transition-transform duration-500 text-menu-item hover:
       <RouterView />
     </main>
 
-    <footer class="bg-menu-background p-10 !text-white flex lg:justify-center">
+    <footer class="text-sm bg-menu-background p-10 !text-white flex lg:justify-center">
       <div class="flex flex-col gap-4 lg:gap-12 lg:flex-row">
         <a class="text-white" href="https://www.instagram.com/saferspace_slam/" target="_blank">
-          <div class="flex gap-[6px] items-center">
+          <div class="flex gap-[10px] items-center">
             <img src="@/assets/instagram_logo_simple.svg" class="h-5 ml-[2px] invert" />
             <p>saferspace_slam</p>
           </div>
         </a>
-        <div class="flex gap-1">
+        <div class="flex gap-2 items-center">
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
             <path
               d="m612-292 56-56-148-148v-184h-80v216l172 172ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q133 0 226.5-93.5T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160Z" />
@@ -106,7 +120,7 @@ const menuItemClasses = "transition-transform duration-500 text-menu-item hover:
           Jeden 3. Mittwoch im Monat um 20:00 Uhr
         </div>
         <a href="https://maps.app.goo.gl/vmScMdnrRQ9NGecB9" class="!text-white" target="_blank">
-          <div class="flex gap-1">
+          <div class="flex gap-2 items-center">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
               <path
                 d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 294q122-112 181-203.5T720-552q0-109-69.5-178.5T480-800q-101 0-170.5 69.5T240-552q0 71 59 162.5T480-186Zm0 106Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z" />
@@ -121,6 +135,10 @@ const menuItemClasses = "transition-transform duration-500 text-menu-item hover:
 
 <style scoped>
 nav a.router-link-exact-active {
-  border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+  box-shadow: 0 1px 0 0 rgba(255, 255, 255, 0.5);
+}
+
+.outline-hover:hover {
+  box-shadow: 0 1px 0 0 rgba(255, 255, 255, 0.8);
 }
 </style>
