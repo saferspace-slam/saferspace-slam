07c8c652142aaf8084f2c1ef8b70409d1717aeea
max <mail@maxgietl.de>
2025-02-23 01:12:44 +0100

    deploy

---
+++ dist/assets/VeranstaltungenPage-CMRlGViS.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-B7aCNMVw.js";const n={};function r(t,c){return null}const a=e(n,[["render",r]]);export{a as default};
--- dist/assets/VeranstaltungenView-DBnJ74E_.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-CpskT-S8.js";const n={};function r(t,c){return null}const s=e(n,[["render",r]]);export{s as default};
